21e2399a5180135a993c71518316c7594dd9e5ba
IVEWOR <howdy@deepslog.com>
2022-05-23 16:28:27 +0530

    odin 01.0 ++

--- 01/index.js
+++ 01/index.js
@@ -1,13 +1,36 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hellow</h1>');
-});
+console.log(`server running at: ${port}`);
 
-server.listen(port, () => {
-  console.log(`server running at: ${port}`);
-});
+const server = http
+  .createServer((req, res) => {
+    let displayPage = './views/';
+
+    // urls
+    if (req.url == '/') {
+      displayPage += 'index.html';
+    } else if (req.url == '/about') {
+      displayPage += 'about.html';
+    } else if (req.url == '/contact') {
+      displayPage += 'contact.html';
+    } else if (req.url == '/assets/styles.css') {
+      displayPage = 'assets/styles.css';
+    } else {
+      displayPage += '404.html';
+    }
+
+    fs.readFile(displayPage, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+      return;
+    });
+  })
+  .listen(port);
